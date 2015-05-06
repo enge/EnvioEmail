ab464fa2e984652b1fb414a8c2087e1e96634f9d
luiz <luiz@luiz-PC>
2015-05-06 19:01:25 -0300

    alteração de margin

--- src/java/com/enge100/ejb/EmailEnvioBean.java
+++ src/java/com/enge100/ejb/EmailEnvioBean.java
@@ -5,7 +5,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -23,35 +22,35 @@ public class EmailEnvioBean {
                         
             Properties prop = System.getProperties();
             
-            prop.put("mail.smtp.host","smtp.googlemail.com");
-            prop.put("mail.smtp.auth","true");
-            prop.put("mail.smtp.port","465");
-            prop.put("mail.smtp.socketFactory.class","javax.net.SSLSocketFactory");
-            prop.put("mail.smtp.socketFactory.port","465");
-            prop.put("mail.smtp.socketFactory.fallback","false");
+            prop.put("mail.smtp.host", "smtp.gmail.com");
+            prop.put("mail.smtp.auth", "true");
+            prop.put("mail.smtp.port", "465");
+            prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            prop.put("mail.smtp.socketFactory.port", "465");
+            prop.put("mail.smtp.socketFactory.fallback", "false");
             
             Session mailSession = Session.getDefaultInstance(prop,null);
             mailSession.setDebug(true);
             
             Message mailMessage = new MimeMessage(mailSession);
-            
             mailMessage.setFrom(new InternetAddress(paraemail));
-            mailMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(paraemail));
+            mailMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
+            
             mailMessage.setContent(mensagem, "text/html");
-            mailMessage.setSubject(senha);
+            mailMessage.setSubject(assunto);
             
             Transport transport = mailSession.getTransport("smtp");
-            transport.connect("smtp.googlemail.com", "lzenge@gmail.com", "l30303030");
+            transport.connect("smtp.gmail.com", senha, assunto);
             transport.sendMessage(mailMessage, mailMessage.getAllRecipients());
+                                  
             
-                     
-                        
-        } catch (MessagingException ex) {
-            Logger.getLogger(EmailEnvioBean.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
+                        mailMessage.setFrom(new InternetAddress(paraemail));
+                    } catch (Exception ex) {
+                        Logger.getLogger(EmailEnvioBean.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+           
+                }
+    }
+
         
     
-    }
-   
-}
